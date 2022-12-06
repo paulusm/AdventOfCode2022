9017ccf26b3280e2c9c0a76132017ed9cc25f06c
Paul Matthews <paulusm@gmail.com>
2022-12-06 07:39:40 +0000

    DAY 3

--- bin/dart_advent.dart
+++ bin/dart_advent.dart
@@ -1,7 +1,7 @@
-import 'package:dart_advent/day01.dart' as day1;
-import 'package:dart_advent/day02.dart' as day2;
+//import 'package:dart_advent/day01.dart' as day1;
+//import 'package:dart_advent/day02.dart' as day2;
+import 'package:dart_advent/day03.dart' as day3;
 
 void main(List<String> arguments) {
-  //day1.Day1();
-  day2.Day2();
+  day3.Day3();
 }
---
+++ lib/day03.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+class Day3 {
+  Day3() {
+    openSacks().then((data) {
+      int Score = 0;
+      data.forEach((String items) {
+        double lenItems = items.length.toDouble();
+        int halfway = (lenItems ~/ 2).toInt();
+        String compt1 = items.substring(0, halfway);
+        String compt2 = items.substring(halfway);
+        Set<String> comptList1 = compt1.split('').toSet();
+        Set<String> comptList2 = compt2.split('').toSet();
+        String overLap = comptList1.intersection(comptList2).first;
+        //Scoring using ASCII Codes
+        int overLapCode = overLap.codeUnitAt(0);
+        Score += (overLapCode > 96 ? overLapCode - 96 : overLapCode - 38);
+      });
+      print('Score of overlapping items - $Score');
+
+      int numGroups = data.length;
+      int Score2 = 0;
+
+      for (int i = 0; i < numGroups; i += 3) {
+        Set<String> membList1 = data[i].split('').toSet();
+        Set<String> membList2 = data[i + 1].split('').toSet();
+        Set<String> membList3 = data[i + 2].split('').toSet();
+        String commonChar =
+            membList1.intersection(membList2).intersection(membList3).first;
+        int overLapCode = commonChar.codeUnitAt(0);
+        Score2 += (overLapCode > 96 ? overLapCode - 96 : overLapCode - 38);
+      }
+      print('Score for teams of 3 - $Score2');
+    });
+  }
+
+  Future<List<String>> openSacks() async {
+    return (await File('data/rucksack.txt').readAsLines());
+  }
+}
