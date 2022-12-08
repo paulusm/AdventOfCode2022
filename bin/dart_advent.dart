58508027c083cd5f442e3a6b1a8bfe185ab1038d
Paul Matthews <paulusm@gmail.com>
2022-12-08 18:40:46 +0000

    close to the edit

--- bin/dart_advent.dart
+++ bin/dart_advent.dart
@@ -3,7 +3,7 @@
 //import 'package:dart_advent/day03.dart' as day3;
 //import 'package:dart_advent/day04.dart' as day4;
 //import 'package:dart_advent/day05.dart' as day5;
-//import 'package:dart_advent/day06.dart' as day6;d
+//import 'package:dart_advent/day06.dart' as day6;
 //import 'package:dart_advent/day07.dart' as day7;
 import 'package:dart_advent/day08.dart' as day8;
 
--- lib/day08.dart
+++ lib/day08.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:matrices/matrices.dart';
 
 class Day8 {
+  Matrix grid = Matrix();
+
   Day8() {
     readData().then((data) {
       List<List<double>> dataList = [];
@@ -15,14 +17,65 @@ class Day8 {
         //print(oneRow.length);
         dataList.add(oneRow);
       }
-      print(dataList.length);
-      print(dataList[0].length);
-      Matrix grid = Matrix.fromList(dataList);
-      print(grid.matrix[0][98]);
+
+      grid = Matrix.fromList(dataList);
+      int numVisible = 0;
+      for (int x = 0; x < grid.matrix.length; x++) {
+        for (int y = 0; y < grid.matrix.length; y++) {
+          numVisible += checkVisible(x, y);
+        }
+      }
+      print('Visble trees = $numVisible');
     });
   }
 
+  int checkVisible(x, y) {
+    List<bool> compass = [false, false, false, false];
+    if (x == 0 ||
+        y == 0 ||
+        x == grid.matrix.length - 1 ||
+        y == grid.matrix.length - 1) {
+      print('perimeter');
+      return 1;
+    }
+
+    int cardCounter = 0;
+
+    for (List cardinal in [
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+      [0, -1]
+    ]) {
+      int a = x;
+      int b = y;
+
+      while (a > 0 &&
+          a < grid.matrix.length - 1 &&
+          b > 0 &&
+          b < grid.matrix.length - 1) {
+        a += cardinal[0] as int;
+        b += cardinal[1] as int;
+
+        if (grid.matrix[a][b] >= grid.matrix[x][y]) {
+          compass[cardCounter] = false;
+          break;
+        }
+      }
+      cardCounter++;
+    }
+    print(compass);
+    if (compass.join() == [false, false, false, false].join()) {
+      print('invis');
+      return 0;
+    } else {
+      //tree is viz
+      print('gotcha!');
+      return 1;
+    }
+  }
+
   Future<List<String>> readData() async {
-    return (await File('data/8-trees.txt').readAsLines());
+    return (await File('data/8-trees-test.txt').readAsLines());
   }
 }
